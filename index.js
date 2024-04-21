558f75070638c1bb78db8f76ff8974c516c43830
unknown <dbdynabeast@gmail.com>
2024-04-21 15:54:54 +0530

    Add .gitignore to exclude node_modules

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-const express = require("express");
-const app = express();
-app.get("/", (req, res) => { res.send("Express on Vercel"); });
+// const express = require("express");
+// const app = express();
+// app.get("/", (req, res) => { res.send("Express on Vercel"); });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
+// const PORT = process.env.PORT || 8080;
+// app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// Use CORS middleware
+app.use(cors());
+
+// Define your routes
+app.get('/', (req, res) =>
+{
+    res.send('Hello, world!');
+});
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () =>
+{
+    console.log(`Server is running on port ${PORT}`);
+});
