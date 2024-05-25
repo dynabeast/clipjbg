1e85599fa66101782b812191be5a78c5804e9e32
unknown <dbdynabeast@gmail.com>
2024-05-25 18:16:55 +0530

    ytcore

--- index.js
+++ index.js
@@ -6,19 +6,19 @@
 // app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
 
 const express = require('express');
-// const cors = require('cors');
-// const ytdl = require('ytdl-core');
+const cors = require('cors');
+const ytdl = require('ytdl-core');
 const app = express();
-// const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
-// const ffmpeg = require('fluent-ffmpeg');
-// ffmpeg.setFfmpegPath(ffmpegPath);
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+const ffmpeg = require('fluent-ffmpeg');
+ffmpeg.setFfmpegPath(ffmpegPath);
 function sanitizeFilename(filename)
 {
     // Remove any invalid characters from the filename
     return filename.replace(/[^\w\.\-\(\)]/g, '_');
 }
 // Use CORS middleware
-// app.use(cors());
+app.use(cors());
 
 
 // app.get('/', async (req, res) =>
@@ -36,78 +36,78 @@ function sanitizeFilename(filename)
 
 // });
 app.get("/", (req, res) => { res.send("Express on Vercel"); });
-// app.get('/download', async (req, res) =>
-// {
-//     const videoUrl = req.query.url; // Get the video URL from the query parameter
+app.get('/download', async (req, res) =>
+{
+    const videoUrl = req.query.url; // Get the video URL from the query parameter
 
-//     if (!videoUrl)
-//     {
-//         return res.status(400).send('Video URL is required');
-//     }
+    if (!videoUrl)
+    {
+        return res.status(400).send('Video URL is required');
+    }
 
-//     try
-//     {
-//         // Get the video info
-//         const info = await ytdl.getInfo(videoUrl);
-//         const videoFormat = ytdl.chooseFormat(info.formats, { quality: 'highest' });
-//         // Sanitize the filename
-//         const filename = sanitizeFilename(info.videoDetails.title);
-//         const sanitizedFilename = `${filename}.${videoFormat.container}`;
+    try
+    {
+        // Get the video info
+        const info = await ytdl.getInfo(videoUrl);
+        const videoFormat = ytdl.chooseFormat(info.formats, { quality: 'highest' });
+        // Sanitize the filename
+        const filename = sanitizeFilename(info.videoDetails.title);
+        const sanitizedFilename = `${filename}.${videoFormat.container}`;
 
 
-//         // Set the Content-Disposition header with the sanitized filename
-//         res.setHeader('Content-Disposition', `attachment; filename="${sanitizedFilename}"`);
-//         res.setHeader('Content-Type', `video/${videoFormat.container}`);
+        // Set the Content-Disposition header with the sanitized filename
+        res.setHeader('Content-Disposition', `attachment; filename="${sanitizedFilename}"`);
+        res.setHeader('Content-Type', `video/${videoFormat.container}`);
 
-//         ytdl(videoUrl, { format: videoFormat }).pipe(res);
-//         // Set the response headers
-//         // res.setHeader('Content-Disposition', `attachment; filename="${info.videoDetails.title}.${videoFormat.container}"`);
-//         // res.setHeader('Content-Type', `video/${videoFormat.container}`);
+        ytdl(videoUrl, { format: videoFormat }).pipe(res);
+        // Set the response headers
+        // res.setHeader('Content-Disposition', `attachment; filename="${info.videoDetails.title}.${videoFormat.container}"`);
+        // res.setHeader('Content-Type', `video/${videoFormat.container}`);
 
-//         // // Stream the video
-//         // ytdl(videoUrl, { format: videoFormat }).pipe(res);
-//     } catch (err)
-//     {
-//         console.error(err);
-//         res.status(500).send('Error downloading the video');
-//     }
-// });
+        // // Stream the video
+        // ytdl(videoUrl, { format: videoFormat }).pipe(res);
+    } catch (err)
+    {
+        console.error(err);
+        res.status(500).send('Error downloading the video');
+    }
+});
 
-// app.get('/audio', async (req, res) =>
-// {
-//     const videoUrl = req.query.url;
-//     if (!videoUrl)
-//     {
-//         return res.status(400).send('Video URL is required');
-//     }
+app.get('/audio', async (req, res) =>
+{
+    const videoUrl = req.query.url;
+    if (!videoUrl)
+    {
+        return res.status(400).send('Video URL is required');
+    }
 
-//     try
-//     {
-//         const info = await ytdl.getInfo(videoUrl);
-//         const audioFormat = ytdl.filterFormats(info.formats, 'audioonly');
-//         const audioStream = ytdl(videoUrl, { format: audioFormat[0] });
+    try
+    {
+        const info = await ytdl.getInfo(videoUrl);
+        const audioFormat = ytdl.filterFormats(info.formats, 'audioonly');
+        const audioStream = ytdl(videoUrl, { format: audioFormat[0] });
 
-//         const encodedFileName = encodeURIComponent(info.videoDetails.title) + '.mp3';
-//         res.setHeader('Content-Disposition', `attachment; filename="${encodedFileName}"`);
-//         res.setHeader('Content-Type', 'audio/mpeg');
+        const encodedFileName = encodeURIComponent(info.videoDetails.title) + '.mp3';
+        res.setHeader('Content-Disposition', `attachment; filename="${encodedFileName}"`);
+        res.setHeader('Content-Type', 'audio/mpeg');
 
-//         const ffmpegProcess = ffmpeg({ source: audioStream })
-//             .audioBitrate(128)
-//             .audioChannels(2)
-//             .toFormat('mp3')
-//             .pipe(res, { end: true });
+        const ffmpegProcess = ffmpeg({ source: audioStream })
+            .audioBitrate(128)
+            .audioChannels(2)
+            .toFormat('mp3')
+            .pipe(res, { end: true });
 
-//         ffmpegProcess.on('error', (err) =>
-//         {
-//             console.error('ffmpeg error:', err);
-//             res.status(500).send('Error converting video to MP3');
-//         });
-//     } catch (err)
-//     {
-//         console.error('Error downloading video:', err);
-//         res.status(500).send('Error downloading video');
-//     }
-// });
+        ffmpegProcess.on('error', (err) =>
+        {
+            console.error('ffmpeg error:', err);
+            res.status(500).send('Error converting video to MP3');
+        });
+    } catch (err)
+    {
+        console.error('Error downloading video:', err);
+        res.status(500).send('Error downloading video');
+    }
+});
 // Start the server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () =>
