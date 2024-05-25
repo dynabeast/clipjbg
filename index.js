1698db386c77414e53d145ab3d9b59b2be51b091
unknown <dbdynabeast@gmail.com>
2024-05-25 22:16:08 +0530

    add api-docs url for resolve issue

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const swaggerOptions = {
 };
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 /**
  * @swagger
  * /video:
